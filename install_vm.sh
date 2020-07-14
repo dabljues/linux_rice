847308187484f59fa3efafe1894405257bbae7f1
dabljues <dabljues96@gmail.com>
2020-07-14 02:38:37 +0200

    Fix VM resizing

--- install_vm.sh
+++ install_vm.sh
@@ -1,8 +1,12 @@
 # Script for VM installation
 
 sh install.sh
-[[ -z "$(grep VBoxClient-all ~/.xprofile)" ]] && echo 'VBoxClient-all' >> ~/.xprofile
+# [[ -z "$(grep VBoxClient-all ~/.xprofile)" ]] && echo 'VBoxClient-all' >> ~/.xprofile
 
-sudo pacman --noconfirm --needed -S virtualbox-guest-iso virtualbox-guest-dkms virtualbox-guest-utils linux-headers > /dev/null 2>&1
-sudo systemctl enable vboxservice.service > /dev/null 2>&1
-VBoxClient-all > /dev/null 2>&1
+sudo pacman --noconfirm --needed -S virtualbox-guest-iso linux-headers  > /dev/null 2>&1
+mkdir -p ~/Media/vbox
+sudo mount -o loop /usr/lib/virtualbox/additions/VBoxGuestAdditions.iso ~/Media/vbox > /dev/null 2>&1
+sudo sh ~/Media/vbox/VBoxLinuxAdditions.run <<< 'yes'
+sudo gpasswd -a $USER vboxsf
+sudo umount ~/Media/vbox
+rm -rf ~/Media
