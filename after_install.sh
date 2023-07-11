6067a49fd38e49b4a84877f7e50229a32fa0afb3
dabljues <45121609+dabljues@users.noreply.github.com>
2023-07-11 08:47:15 +0200

    Dark theme for chromium

--- after_install.sh
+++ after_install.sh
@@ -7,3 +7,6 @@ sudo pacman -Rns vim
 # pywal
 wal -i images/ocean.jpg
 # add wal -R to xinitrc
+
+# Set dark theme (for example for Chromium)
+dconf write /org/gnome/desktop/interface/color-scheme \'prefer-dark\'
